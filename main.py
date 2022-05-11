575ed56fb2b3b52df2072aab451e35af3b748826
simlouis <slouis@uvm.edu>
2022-05-11 16:43:46 -0400

    Progress

--- main.py
+++ main.py
@@ -8,20 +8,12 @@ router1 = "169.254.20.158"
 router2 = "169.254.173.130"
 router3 = "169.254.240.121"
 
+port_list = []
+graph = [[]]
+
 
 def connect():
-    # cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # host = input("Please enter host name")
     host = "www.goatgoose.com"
-    # port = 2222
-    #
-    # try:
-    #     cl.connect((host, port))
-    # except Exception as e:
-    #     print(e)
-    #
-    # print("Connected to {} on port {}".format(host, port))
 
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=0.5)
@@ -33,8 +25,7 @@ def connect():
     data = None
 
     try:
-        test = session.get("http://{}:2222/get_topology/{}".format(host, endpoint))
-        data = test.json()
+        data = session.get("http://{}:2222/get_topology/{}".format(host, endpoint)).json()
     except Exception as e:
         print(e)
 
@@ -43,23 +34,79 @@ def connect():
 
 
 def compute_optimal(data):
-    # print(data)
-    # src = router1
-    # dest = router2
-
-    nodes = []
+    high_num_vertices = 0
+    src_node = 0
+    dest_node = 0
 
     for i in data.values():
         for j in i:
-            if j[0] not in nodes:
-                nodes.append(j[0])
+            edge = j
+            source_node = 0
 
-    print(nodes)
+            try:
+                source_node = int(edge[0])
+            except Exception as e:
+                source_node = high_num_vertices
+            if source_node > high_num_vertices:
+                high_num_vertices = source_node
 
-    # dykstras algorithm to compute forwarding tables
+    high_num_vertices += 3
 
-    pass
+    # for i in range(high_num_vertices):
+    #     graph.append()
 
+    for i in data.values():
+        for node in i:
+            # source_ip_tuple = ()
+            # dest_ip_tuple = ()
+            out_port_int = int(node[2])
+
+            if isinstance(node[0], int):
+                source_ip = node[0]
+                source_ip_tuple = (str(source_ip), source_ip - 1)
+            else:
+                source_ip = str(node[0])
+                source_ip_int = 0
+                try:
+                    source_ip = int(source_ip)
+                except Exception as e:
+                    if source_ip == router1:
+                        source_ip_int = high_num_vertices
+                    elif source_ip == router2:
+                        source_ip_int = high_num_vertices - 1
+                    elif source_ip == router3:
+                        source_ip_int = high_num_vertices - 2
+
+                source_ip_tuple = (str(source_ip), source_ip_int - 1)
+                source_node = source_ip_tuple[1]
+
+            if isinstance(node[1], int):
+                dest_ip = node[1]
+                dest_ip_tuple = (str(dest_ip), dest_ip - 1)
+
+            elif isinstance(node[1], str):
+                dest_ip = node[1]
+                dest_ip_int = 0
+                try:
+                    dest_ip_int = int(dest_ip)
+                except Exception as e:
+                    if dest_ip == router1:
+                        dest_ip_int = high_num_vertices
+                    elif dest_ip == router2:
+                        dest_ip_int = high_num_vertices - 1
+                    elif dest_ip == router3:
+                        dest_ip_int = high_num_vertices - 2
+
+                dest_ip_tuple = (str(dest_ip), dest_ip_int - 1)
+                dest_node = dest_ip_tuple[1]
+
+            port_list.append((source_ip_tuple[0], source_ip_tuple[1], dest_ip_tuple[0], dest_ip_tuple[1], out_port_int))
+            add_edge(graph, source_ip_tuple[1], dest_ip_tuple[1])
+
+
+def add_edge(graph, src, dest):
+    graph[src].append(dest)
+    graph[dest].append(src)
 
 def send_tables():
 
